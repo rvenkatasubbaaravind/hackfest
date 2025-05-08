0b7fafe8d783acaccb22c10b4e00cd953815d53e
Akhil Biju <abiju@infoblox.com>
2025-05-08 17:19:32 +0530

    feat(ui): update mocks & wait script

--- backstop_data/engine_scripts/waitForAPIs.js
+++ backstop_data/engine_scripts/waitForAPIs.js
@@ -5,4 +5,17 @@ module.exports = async (page, scenario) => {
 
   console.log("Waiting for all ib-spinner elements to have empty children...");
   console.log("All ib-spinner elements have empty children.");
+
+  await page.evaluate(() => {
+    const legendItemDiv = document.querySelector(
+      "ib-nested-sunburst-chart .legend-items .legend-item:first-child"
+    );
+    if (legendItemDiv) {
+      legendItemDiv.click();
+      console.log("Clicked on the legend item div.");
+    } else {
+      console.log("Legend item div not found.");
+    }
+  });
+  console.log("Click action completed.");
 };
