6d94fa3dbe76acb2e808a6c58f06922dc6152a04
Akhil Biju <abiju@infoblox.com>
2025-05-08 10:43:19 +0530

    feat(backstopjs): refine wait logic

--- backstop_data/engine_scripts/waitForAPIs.js
+++ backstop_data/engine_scripts/waitForAPIs.js
@@ -1,13 +1,16 @@
 module.exports = async (page, scenario) => {
 
-  console.log('Waiting for 3 seconds...');
-  await new Promise(resolve => setTimeout(resolve, 3000));
-  console.log('3 seconds wait completed.');
+  console.log('Waiting for any ib-spinner element to have at least one child...');
+  await page.waitForFunction(() => {
+    const spinners = document.querySelectorAll('ib-spinner');
+    return Array.from(spinners).some(spinner => spinner.children.length > 0);
+  });
+  console.log('An ib-spinner element now has at least one child.');
 
-  console.log('Waiting for all ib-spinner elements to have empty children...');
+  console.log('Waiting for all ib-spinner elements to have no children...');
   await page.waitForFunction(() => {
     const spinners = document.querySelectorAll('ib-spinner');
     return Array.from(spinners).every(spinner => spinner.children.length === 0);
   });
-  console.log('All ib-spinner elements have empty children.');
+  console.log('All ib-spinner elements now have no children.');
 };
